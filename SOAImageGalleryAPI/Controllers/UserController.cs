314d8d6cfb550e2ac4608bd11261044833843571
M2113 <m2113@student.jamk.fi>
2021-03-20 16:04:06 +0200

    added few user routes

--- SOAImageGalleryAPI/Controllers/UserController.cs
+++ SOAImageGalleryAPI/Controllers/UserController.cs
@@ -1,16 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ConsoleApp.PostgreSQL;
+using Microsoft.AspNetCore.Mvc;
+using SOAImageGalleryAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System.Text;
+using SOAImageGalleryAPI.Wrappers;
 
 namespace SOAImageGalleryAPI.Controllers
 {
+    [Route("[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private DataContext _context = null;
+        public UserController(DataContext context)
         {
-            return View();
+            _context = context;
         }
+
+        [HttpPost("/register")]
+        public ActionResult RegisterUser([FromBody] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.UserId = Guid.NewGuid().ToString();
+                user.UserPassword = BCrypt.Net.BCrypt.HashPassword(user.UserPassword);
+                user.Created = DateTime.Now;
+                user.Updated = DateTime.Now;
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("/login")]
+        public IActionResult LogInUser([FromBody] User user)
+        {
+            var fetchedUser = _context.Users.FirstOrDefault(i => i.UserName == user.UserName);
+            if(fetchedUser == null) { return BadRequest(); }
+
+            if (!BCrypt.Net.BCrypt.Verify(user.UserPassword, fetchedUser.UserPassword))
+            {
+                return BadRequest();
+            }
+            return Ok(new Response<User>(fetchedUser));
+        }
+
+
     }
 }
