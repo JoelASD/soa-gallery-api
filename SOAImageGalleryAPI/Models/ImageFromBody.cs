bf25d8977cc2ea0fb401495e034024411612292a
Samson Azizyan <samson.azizyan@liveto.io>
2021-03-27 13:14:00 +0200

    ImageFromBody class, Image upload to MinIO and database using base64 string

---
+++ SOAImageGalleryAPI/Models/ImageFromBody.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SOAImageGalleryAPI.Models
+{
+    [Keyless]
+    public class ImageFromBody
+    {
+        public string ImageFile { get; set; }
+        public string ImageFileExtension { get; set; }
+        public string ImageTitle { get; set; }
+        public string UserID { get; set; }
+    }
+}
